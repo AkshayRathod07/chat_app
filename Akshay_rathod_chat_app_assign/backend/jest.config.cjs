adde91eb7a2d030d604909fbec369a987b05fe67
Akshay <rathodak315@gmail.com>
2026-02-19 23:43:03 +0530

    removed unwanted folder

--- Akshay_rathod_chat_app_assign/backend/jest.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/tests'],
-    testRegex: '\\.(test|spec)\\.ts$',
-    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-};
